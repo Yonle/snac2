a66bbd4360ebf8b97872376e2ea0c2e7933c3e56
default <nobody@localhost>
2022-09-29 12:52:57 +0200

    Use SIGTRAP instead of the signal number in raise().

--- xs.h
+++ xs.h
@@ -38,7 +38,7 @@ typedef char d_char;
 #define _xs_blk_size(sz) ((((sz) + _XS_BLK_SIZE) / _XS_BLK_SIZE) * _XS_BLK_SIZE)
 
 void _xs_destroy(char **var);
-#define xs_debug() raise(5)
+#define xs_debug() raise(SIGTRAP)
 xstype xs_type(const char *data);
 int xs_size(const char *data);
 int xs_is_null(char *data);
