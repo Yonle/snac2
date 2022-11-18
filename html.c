e239d51de1a0158460defc198fe7fbc7f8049b0a
default <nobody@localhost>
2022-11-18 11:36:13 +0100

    More RSS tweaks.

--- html.c
+++ html.c
@@ -1124,7 +1124,9 @@ int html_get_handler(d_char *req, char *q_path, char **body, int *b_size, char *
             content = xs_replace_i(content, ">", "&gt;");
 
             /* cut title in the first tag start */
-            if ((v = strchr(title, '&')) || (v = strchr(title, '<')))
+            if ((v = strchr(title, '<')))
+                *v = '\0';
+            if ((v = strchr(title, '&')))
                 *v = '\0';
 
             if (strlen(title) > 40) {
