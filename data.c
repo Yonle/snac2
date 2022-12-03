3929778ea1e37c48d4d94a804f5dd32bcf89b926
default <nobody@localhost>
2022-12-03 20:28:23 +0100

    Don't store messages in public/ that are not public.
    They weren't shown anyway, but it's better to be sure.

--- data.c
+++ data.c
@@ -780,8 +780,15 @@ void timeline_update_indexes(snac *snac, const char *id)
 {
     object_user_cache_add(snac, id, "private");
 
-    if (xs_startswith(id, snac->actor))
-        object_user_cache_add(snac, id, "public");
+    if (xs_startswith(id, snac->actor)) {
+        xs *msg = NULL;
+
+        if (valid_status(object_get(id, &msg, NULL))) {
+            /* if its ours and is public, also store in public */
+            if (is_msg_public(snac, msg))
+                object_user_cache_add(snac, id, "public");
+        }
+    }
 }
 
 
