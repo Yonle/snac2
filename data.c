30e1a8ce085c91186702ea7149dbab305d912cfb
default <nobody@localhost>
2022-12-03 19:37:52 +0100

    Add this user admires to the public timeline.

--- data.c
+++ data.c
@@ -800,6 +800,10 @@ int timeline_add(snac *snac, char *id, char *o_msg, char *parent, char *referrer
 void timeline_admire(snac *snac, char *o_msg, char *id, char *admirer, int like)
 /* updates a timeline entry with a new admiration */
 {
+    /* if we are admiring this, add to the public timeline */
+    if (!like && strcmp(admirer, snac->actor) == 0)
+        object_user_cache_add(snac, id, "public");
+
     object_admire(id, admirer, like);
 
     snac_debug(snac, 1, xs_fmt("timeline_admire (%s) %s %s",
