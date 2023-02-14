e144b0dc67838c99fcfc2c4db75ac0ac54535b2d
default <nobody@localhost>
2023-02-14 08:29:21 +0100

    Add OK or ERROR to HTTP response.

--- xs_httpd.h
+++ xs_httpd.h
@@ -265,7 +265,7 @@ void xs_httpd_response(FILE *f, int status, xs_dict *headers, xs_str *body, int 
     xs_str *k;
     xs_val *v;
 
-    proto = xs_fmt("HTTP/1.1 %d ", status);
+    proto = xs_fmt("HTTP/1.1 %d %s", status, status / 100 == 2 ? "OK" : "ERROR");
     fprintf(f, "%s\r\n", proto);
 
     p = headers;
