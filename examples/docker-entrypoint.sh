5b7a22e5b01608d046a665bb1a16dd4b4785e507
default <nobody@localhost>
2023-05-05 12:58:54 +0200

    Updated examples/docket-entrypoint.sh.
    snac init now prompts for an optional admin email address.

--- examples/docker-entrypoint.sh
+++ examples/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 if [ ! -e /data/data/server.json ]
 then
-    echo -ne "0.0.0.0\r\n8001\r\nlocalhost\r\n\r\n" | snac init /data/data
+    echo -ne "0.0.0.0\r\n8001\r\nlocalhost\r\n\r\n\r\n" | snac init /data/data
     snac adduser /data/data testuser
 fi
 SSLKEYLOGFILE=/data/key snac httpd /data/data
