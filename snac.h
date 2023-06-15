57761020b71161fbed85079e349b8d222d931042
default <nobody@localhost>
2023-06-15 18:28:24 +0200

    Version 2.35 RELEASED.

--- snac.h
+++ snac.h
@@ -1,7 +1,7 @@
 /* snac - A simple, minimalistic ActivityPub instance */
 /* copyright (c) 2022 - 2023 grunfink / MIT license */
 
-#define VERSION "2.35-dev"
+#define VERSION "2.35"
 
 #define USER_AGENT "snac/" VERSION
 
